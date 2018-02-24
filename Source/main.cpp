983bad86b58a574cb40cd36232c874b088ed6a21
unknown <DennyLindberg@users.noreply.github.com>
2018-02-24 20:05:45 +0100

    Started work on classes for storing image info and storage size

--- Source/main.cpp
+++ Source/main.cpp
@@ -3,6 +3,78 @@
 //#include <string_view>
 #include <regex>
 #include <vector>
+#include <assert.h>
+
+enum class ImageType { JPEG, JPEG2000, BMP };
+class ImageInfo
+{
+public:
+	ImageType type = ImageType::BMP;
+	unsigned int width = 0;
+	unsigned int height = 0;
+
+public:
+	ImageInfo(ImageType imageType, unsigned int imageWidth, unsigned int imageHeight)
+		: type{ imageType }, width{ imageWidth }, height{ imageHeight }
+	{}
+
+	~ImageInfo() = default;
+
+	unsigned int GetSizeInBytes()
+	{
+		switch (type)
+		{
+		case ImageType::JPEG2000:
+			return (unsigned int) (width * height * 0.4 / log(log(width * height + 16)));
+
+		case ImageType::JPEG:
+		case ImageType::BMP:
+		default:
+			return CalculatePyramidSizeInBytes(width, height);
+		}
+	}
+
+private:
+	unsigned int CalculatePyramidSizeInBytes(unsigned int baseWidth, unsigned int baseHeight)
+	{
+		const int minimumDimension = 128;
+		unsigned int totalSize = 0;
+		do
+		{
+			switch (type)
+			{
+			case ImageType::JPEG:
+				totalSize += (unsigned int) (width * height * 0.2);
+				break;
+
+			case ImageType::BMP:
+			default:
+				totalSize += width * height;
+				break;
+			}
+
+			baseWidth /= 2;
+			baseHeight /= 2;
+		} while (baseWidth >= minimumDimension && baseHeight >= minimumDimension);
+
+		return totalSize;
+	}
+};
+
+class ImageInfoStack
+{
+private:
+	std::vector<ImageInfo> stack;
+
+public:
+	ImageInfoStack() = default;
+	~ImageInfoStack() = default;
+
+	void Push(ImageInfo& newImageInfo)
+	{
+
+	}
+};
 
 template <class T>
 void PrintVector(const std::vector<T>& vec)
@@ -22,35 +94,25 @@ void PrintVector(const std::vector<T>& vec)
 	}
 }
 
-void TokenizeString(const std::string &str, std::vector<std::string>& tokens)
+void SplitStringUsingRegex(const std::string &str, std::vector<std::string>& tokens, const std::regex expression)
 {
-	const std::regex findWhitespaces("\\s+");
-	std::sregex_token_iterator tokensIterator(str.begin(), str.end(), findWhitespaces, -1);
-	tokens.assign(tokensIterator, std::sregex_token_iterator());
+	std::sregex_token_iterator tokensBegin(str.begin(), str.end(), expression, -1);
+	std::sregex_token_iterator tokensEnd;
+	tokens.assign(tokensBegin, tokensEnd);
 }
 
 bool ValidateCommand(const std::string &command)
 {
 	std::vector<std::string> validCommands = {
-		"Q",				// Quit (finish input)
-		"G",				// Image Group (Stack)
+		"Q",				// Quit (end of input)
+		"G",				// Image Group (stack)
 		"J", "JPEG",		// Image formats
 		"JP2", "JPEG200",
 		"BMP"
 	};
 
 	bool isValidCommand = (std::find(validCommands.begin(), validCommands.end(), command) != validCommands.end());
-	if (!isValidCommand)
-	{
-		std::cout << "\n The input [" << command << "] is not a valid command!\n\n";
-	}
-
 	return isValidCommand;
-}
-
-bool ValidateInput(const std::vector<std::string>& inputTokens)
-{
-	return (inputTokens.size() > 0) && ValidateCommand(inputTokens[0]);
 }
 
 int main()
@@ -70,21 +132,30 @@ Exit with "Q"
 )";
 
 	std::vector<std::string> inputTokens;
+	const std::regex matchWhitespaces("\\s+");
 	while (true)
 	{
 		std::cout << "Add image/group: ";
 		std::getline(std::cin, userInput);
 
-		if (userInput == "Q") break;
+		if (userInput.size() == 0)	continue;
+		if (userInput == "Q")		break;
 
-		TokenizeString(userInput, inputTokens);
-		if (ValidateInput(inputTokens))
+		SplitStringUsingRegex(userInput, inputTokens, matchWhitespaces);
+		std::string command = inputTokens[0];
+
+		if (!ValidateCommand(command))
 		{
-			images.push_back(userInput);
+			std::cout << "\n The input [" << command << "] is not a valid command!\n\n";
 		}
+		else
+		{
+
+		}
+		//images.push_back(userInput);
 	}
 	
-	std::getline(std::cin, userInput);
+	//std::getline(std::cin, userInput);
 
     return 0;
 }
